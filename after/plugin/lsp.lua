dd9d544a618149a7f8a718b97cc0d2483ffa4c34
Admiral-Simo <admiralsimo16@gmail.com>
2024-02-06 20:32:40 +0100

    preference shortcut

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -53,7 +53,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("i", "<C-b>", function() vim.lsp.buf.signature_help() end, opts)
 end)
 
 -- Set up LSP
