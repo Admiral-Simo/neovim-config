f20b1cd45c0e9dc53b243e9f1419c424f0092f2f
Admiral-Simo <admiralsimo16@gmail.com>
2024-06-01 19:09:55 +0100

    feature: ignore .gitignore in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,20 @@
+require('telescope').setup{
+  defaults = {
+    -- This will use your .gitignore to ignore files
+    vimgrep_arguments = {
+      'rg',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--column',
+      '--smart-case',
+      '--glob=!{.git,node_modules}/*',
+    },
+    file_ignore_patterns = {'.git/', 'node_modules/'},
+  },
+}
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
