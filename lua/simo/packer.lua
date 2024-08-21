38c6af64286785ec2bbb3800a7a7fc68306e3536
Admiral-Simo <admiralsimo16@gmail.com>
2024-08-21 17:02:24 +0100

    remove codium

--- lua/simo/packer.lua
+++ lua/simo/packer.lua
@@ -90,16 +90,4 @@ return require("packer").startup(function(use)
     })
 
     use("tjdevries/templ.nvim")
-
-    use {
-        "Exafunction/codeium.nvim",
-        requires = {
-            "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp",
-        },
-        config = function()
-            require("codeium").setup({
-            })
-        end
-    }
 end)
